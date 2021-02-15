da1c79102d5226cb210884ce2ecc9a55ec622e28
Yacob (Kobi) Cohen-Arazi <kobi.cohenarazi@gmail.com>
2021-02-15 08:20:17 -0800

    improve c++ version
    cbegin, ELEM/CONT

--- main.cpp
+++ main.cpp
@@ -9,9 +9,9 @@
 // The input string will only consist of lower case letters and/or spaces.
 // https://www.codewars.com/kata/54ff3102c1bad923760001f3?utm_source=newsletter
 
-template <typename E, typename C>
-bool is_in(const E &e, const C &c) {
-    return std::any_of(std::begin(c), std::end(c), [e](const auto chr){ return chr == e; });
+template <typename ELEM, typename CONT>
+bool is_in(const ELEM &e, const CONT &c) {
+    return std::any_of(std::cbegin(c), std::cend(c), [e](const auto chr){ return chr == e; });
 }
 
 
@@ -19,9 +19,10 @@ int main(int, char*[]) {
     const std::string input("hello world");
     const std::vector<char> chrs{'a', 'e', 'i', 'o', 'u'};
 
-    const auto has_vowels([&chrs](const auto c){ return is_in(c, chrs); });
-    std::clog << std::count_if(std::begin(input), std::end(input), has_vowels) << '\n';
+    const auto has_vowel([&chrs](const auto c){ return is_in(c, chrs); });
+    std::clog << std::count_if(std::cbegin(input), std::cend(input), has_vowel) << '\n';
 
-    const auto cnt = ranges::distance(input | ranges::views::filter(has_vowels));
+    // with ranges
+    const auto cnt = ranges::distance(input | ranges::views::filter(has_vowel));
     std::clog << cnt << '\n';
 }
